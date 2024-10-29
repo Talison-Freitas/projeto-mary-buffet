318688b5472c901d71dce0769f075b64e9a82299
Talison-Freitas <Talisondefreitas@outlook.com>
2024-10-28 23:17:28 -0300

    fix: correção no caminho dos links vinculados aos botões

--- script/script.js
+++ script/script.js
@@ -2,32 +2,32 @@
 const btnMobile = document.getElementById("btnMobile");
 
 function toggleMenu(event) {
-  if (event.type === 'touchstart') event.preventDefault()
+  if (event.type === "touchstart") event.preventDefault();
   const nav = document.getElementById("nav");
-  nav.classList.toggle('active');
-   const active = nav.classList.contains('active');
-  event.currentTarget.setAttribute('aria-expanded', active);
-  if(active) {event.currentTarget.setAttribute('aria-label', "Fechar Menu")
+  nav.classList.toggle("active");
+  const active = nav.classList.contains("active");
+  event.currentTarget.setAttribute("aria-expanded", active);
+  if (active) {
+    event.currentTarget.setAttribute("aria-label", "Fechar Menu");
   } else {
-event.currentTarget.setAttribute('aria-label', 'Abrir Menu')}
-
+    event.currentTarget.setAttribute("aria-label", "Abrir Menu");
+  }
 }
 
-btnMobile.addEventListener('click', toggleMenu);
-btnMobile.addEventListener('touchstart', toggleMenu);
-
+btnMobile.addEventListener("click", toggleMenu);
+btnMobile.addEventListener("touchstart", toggleMenu);
 
 // Animação após carregamento
 
 function apareceElemento() {
   const introducaoConteudo = document.querySelector(".introducao-conteudo");
-  if (introducaoConteudo){
-  introducaoConteudo.classList.add("surgeEsquerda");
+  if (introducaoConteudo) {
+    introducaoConteudo.classList.add("surgeEsquerda");
   }
   const introducaoImagem = document.querySelector(".introducao-imagem");
-if (introducaoImagem) {
-  introducaoImagem.classList.add("surgeDireita");
-}
+  if (introducaoImagem) {
+    introducaoImagem.classList.add("surgeDireita");
+  }
 }
 
-window.addEventListener('load', apareceElemento);
+window.addEventListener("load", apareceElemento);
